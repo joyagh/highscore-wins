581f292803d405de7c53aa63fe25c28fc1b5afbd
Justine Oyaghiro <justineoyaghiro@icloud.com>
2026-04-14 09:56:59 -0400

    Initial commit

---
+++ src/main/java/com/pluralsight/HighScore.java
@@ -0,0 +1,15 @@
+package com.pluralsight;
+
+
+import java.time.LocalDate;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class HighScore {
+    public static void main(String[] args) {
+
+        System.out.println("Please enter scores.");
+
+
+    }
+}
