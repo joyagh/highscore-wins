92919b031844f02589a6af9e0b6c5d21b00fdf81
Justine Oyaghiro <justineoyaghiro@icloud.com>
2026-04-14 11:29:08 -0400

    added score parsers

--- src/main/java/com/pluralsight/HighScore.java
+++ src/main/java/com/pluralsight/HighScore.java
@@ -10,14 +10,24 @@ public class HighScore {
 
         Scanner input = new Scanner(System.in);
         System.out.print("Please enter scores: ");
-        String highScore = input.next();
+        String highScore = input.next().toUpperCase();
 
-        String[] score = highScore.split(Pattern.quote("|"));
+        String[] score = highScore.split(Pattern.quote("|").trim());
         String[] team = score[0].split(":");
         String[] totalScores = score[1].split(":");
 
         String home = team[0];
-        String visitor = team[1];
+        String visitors = team[1];
+
+        int scoreHome = Integer.parseInt(totalScores[0]);
+        int visitorScore = Integer.parseInt(totalScores[1]);
+
+
+        if (scoreHome > visitorScore){
+            System.out.println("The winner is: " + home);
+        } else if (visitorScore > scoreHome) {
+            System.out.println("The winner is: " + visitors );
+        }
 
 
 
